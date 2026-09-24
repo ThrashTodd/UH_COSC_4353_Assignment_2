bacd16a8f077dbb72e114f5a9babed77e8158cdc
Todd Thrash <thrashtodd2.8@gmail.com>
2026-09-24 17:11:31 -0500

    Saving progress, need to redo some of index.html and register.html fo the javascript

--- login.js
+++ login.js
@@ -1,34 +1,39 @@
 
 
-const users = [
-    {
-        email: "admin@example.com",
-        password: "admin123",
-        role: "admin"
-    },
-    {
-        email: "user1@example.com",
-        password: "user1_123",
-        role: "user"
-    },
-    {
-        email: "user2@example.com",
-        password: "user2_123",
-        role: "user"
-    }
-]
+const usersMap = new Map([
+    // map of users for const lookup
+    // username : email, password, role
+    [
+        "admin1",
+        { email: "admin@example.com", password: "admin123", role: "admin" }
+    ],
+    [
+        "user1",
+    { email: "user1@example.com", password: "user1_123", role: "user" }
+    ],
+    [
+        "user2",
+        { email: "user2@example.com", password: "user2_123", role: "user" }
+    ]
+])
 
-localStorage.setItem("users", JSON.stringify(users));
+// turns map into array to be stored as string
+localStorage.setItem("users", JSON.stringify([...usersMap]));
 
-function addNewUser(newEmail, newPassword, newRole= "user"){
-    let addUserList = JSON.parse(localStorage.getItem("users")) || [];
 
-    addUserList.push({
-        email: newEmail,
-        password: newPassword,
-        role: newRole
-    })
-    localStorage.setItem("users", JSON.stringify(addUserList))
+function addNewUser( userName, newEmail, newPassword, newRole= "user") {
+    // function to add a new user
+
+    // takes users from local storage and turns it into map
+    let addUserMap = new Map(JSON.parse(localStorage.getItem("users")))
+
+    addUserMap.set(
+        userName, 
+    { email: newEmail, password: newPassword, role: newRole }
+    )
+
+    // changes map into array to be stored as string
+    localStorage.setItem("users", JSON.stringify([...addUserMap]));
 }
 
 
@@ -39,10 +44,10 @@ const newPassword = document.getElementById("password")
 const checkPassword = document.getElementById("confirmPassword")
 
 
-/*  FIX ME
 
+/*
 resgisterForm.addEventListener("submit", function(event)) {
-    event.preventDefault()
+    #event.preventDefault()
 
     const typedEmail = newEmail.value;
     const typedPassword = typedPassword.value;

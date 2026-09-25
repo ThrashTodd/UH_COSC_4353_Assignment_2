6e02b478c6db725ff2497ba90d18f576e1524234
Todd Thrash <thrashtodd2.8@gmail.com>
2026-09-24 19:10:06 -0500

    Rewrote index.html and register.html to work with the login.js. Added validation for having required fields.

--- login.js
+++ login.js
@@ -38,23 +38,92 @@ function addNewUser( userName, newEmail, newPassword, newRole= "user") {
 
 
 
-const resgisterForm = document.getElementById("register_form")
-const newEmail = document.getElementById("email")
-const newPassword = document.getElementById("password")
-const checkPassword = document.getElementById("confirmPassword")
+const form = document.getElementById("form")
+const usernameInput = document.getElementById("usernameInput")
+const emailInput = document.getElementById("emailInput")
+const passwordInput = document.getElementById("passwordInput")
+const checkPasswordInput = document.getElementById("confirmPassword")
+const errorMessage = document.getElementById("errorMessage")
 
-
-
-/*
-resgisterForm.addEventListener("submit", function(event)) {
-    #event.preventDefault()
-
-    const typedEmail = newEmail.value;
-    const typedPassword = typedPassword.value;
-    const typedConfirmPassword = typedConfirmPassword.value;
-
-    addNewUser(typedEmail, typedPassword)
-
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    // array of errors
+    let errors = []
     
+    if(emailInput){
+
+        // if we have a email then we're on the registeration page
+        errors = getRegisterErrors(usernameInput.value, emailInput.value, passwordInput.value, checkPasswordInput.value)
+    }
+    else{
+        // else we're on the login page
+        errors = getLoginErrors(usernameInput.value, passwordInput.value)
+    }
+    if(errors.length > 0){
+        //e.preventDefault()
+        errorMessage.innerText = errors.join(". ")
+    } else {
+        errorMessage.innerText = ""
+
+        if(emailInput){
+            window.location.href= "index.html"
+        } else {
+            window.location.href= "user.html"
+        }
+    }
+    
+})
+
+// add user already taken
+function getRegisterErrors(username, email, password, checkPassword){
+    let errors = []
+
+    if(username === '' || username == null){
+        errors.push("Username is required")
+        usernameInput.parentElement.classList.add("incorrect")
+    }
+    if(email === '' || !email){
+        errors.push("Email is required")
+        emailInput.parentElement.classList.add("incorrect")
+    }
+    if(password === '' || !password){
+        errors.push("Password is required")
+        passwordInput.parentElement.classList.add("incorrect")
+    }
+    if(password.length < 8){
+        errors.push("Password must be at least 8 characters")
+        passwordInput.parentElement.classList.add("incorrect")
+    }
+    if(password != checkPassword){
+        errors.push("Passwords don't match")
+        passwordInput.parentElement.classList.add("incorrect")
+        checkPasswordInput.parentElement.classList.add("incorrect")
+
+    }
+return errors
 }
-*/
+// add password matching, no user found 
+function getLoginErrors(username, password){
+    let errors = []
+
+    if(username === '' || !username){
+        errors.push("Username is required")
+        usernameInput.parentElement.classList.add("incorrect")
+    }
+    if(password === '' || !password){
+        errors.push("Password is required")
+        passwordInput.parentElement.classList.add("incorrect")
+}
+return errors
+}
+
+const allInputs = [usernameInput, emailInput, passwordInput, checkPasswordInput].filter(input => input != null)
+
+allInputs.forEach(input => {
+    input.addEventListener("input", () => {
+        if(input.parentElement.classList.contains("incorrect")){
+            input.parentElement.classList.remove("incorrect")
+            errorMessage.innerText = ""
+        }
+    })
+})
